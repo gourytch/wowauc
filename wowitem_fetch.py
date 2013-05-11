ab2e917383fd81b2a38b892982602e2e5ab39a7d
Lev V. Babchenko <gour@mail.ru>
2013-05-11 14:14:30 +0600

    wowitem хранит теперь высосанное из wowapi в виде anydbm-файла

--- wowitem_fetch.py
+++ wowitem_fetch.py
@@ -7,6 +7,10 @@ import cStringIO
 import time
 import os.path
 import sys
+import anydbm
+import re
+import glob
+import shutil
 
 exec file("wowauc.conf", "rt").read()
 
@@ -19,8 +23,145 @@ locales = items_locales.split()
 
 CURDIR = os.path.dirname(os.path.abspath(__file__)) + "/"
 SAVEDIR = CURDIR + dir_fetched_items + "/"
+DBFILE = re.sub(r"/+$", "", os.path.abspath(SAVEDIR)) + ".anydbm"
 
 QUIET   = '-q' in sys.argv
+
+db = None # value will initialized in init_d()
+
+class FS_Base:
+    
+    def __init__(self, basedir):
+        self.open(basedir)
+        return
+        
+    def open(self, basedir):
+        self.basedir = basedir
+        return
+        
+        
+    def close(self):
+        return
+    
+        
+    def has_item(self, id, loc):
+        if type(id) not in (int, long): id = int(id)
+        fname = "%s/%06d-%s.json" % (self.basedir, id, loc)
+        return os.path.exists(fname)
+
+
+    def put(self, id, loc, json):
+        if type(id) not in (int, long): id = int(id)
+        fname = "%s/%06d-%s.json" % (self.basedir, id, loc)
+        file(fname, "wt").write(json)
+
+
+    def get(self, id, loc):
+        if not self.has_item(id, loc): return None
+        if type(id) not in (int, long): id = int(id)
+        fname = "%s/%06d-%s.json" % (self.basedir, id, loc)
+        return file(fname, "rt").read()
+
+
+    def keys(self):
+        x = re.compile(r".*/(\d+)-(\w+)\.json$")
+        R = []
+        for k in glob.glob("%s/*.json" % self.basedir):
+            rx = x.search(k)
+            if not rx:
+                print "BAD KEY: {%s}" % k
+                continue
+            R.append({'id': int(rx.group(1)), 'loc': rx.group(2)})
+        return R
+
+###
+   
+class D_Base:
+    
+    
+    def __init__(self, dbfile):
+        self.open(dbfile)
+        return
+        
+
+    def backup_file(self):
+        S = ('.5','.4','.3','.2','.1', '')
+        for i in range(len(S) - 1):
+            dst = self.fname + S[i]
+            src = self.fname + S[i + 1]
+            if os.path.exists(src):
+                if os.path.exists(dst):
+                    remove(dst)
+                if src == self.fname:
+                    shutil.copy2(src, dst)
+                else:
+                    shutil.move(src, dst)
+        return
+    
+    
+    def open(self, fname):
+        self.fname = fname
+        self.backup_file()
+        self.db = anydbm.open(self.fname, "c", 0666)
+        return
+
+
+    def close(self):
+        self.db.sync()
+        self.db.close()
+        return
+    
+    
+    def has_item(self, id, loc):
+        if type(id) not in (int, long): id = int(id)
+        key ="%06d-%s" % (id, loc)
+        return self.db.has_key(key)
+
+
+    def put(self, id, loc, json):
+        if type(id) not in (int, long): id = int(id)
+        key ="%06d-%s" % (id, loc)
+        self.db[key] = json
+#        self.db.sync()
+        return
+
+
+    def get(self, id, loc):
+        if type(id) not in (int, long): id = int(id)
+        key ="%06d-%s" % (id, loc)
+        return self.db.get(key, None)
+
+
+    def keys():
+        x = re.compile(r"^(\d{6})-(\w+)$")
+        R = []
+        for k in self.db.keys():
+            rx = x.search(k)
+            if not rx:
+                print "BAD KEY: {%s}" % k
+                continue
+            R.append({'id': int(rx.group(1)), 'loc': rx.group(2)})
+        return R
+    
+####################################################################
+
+
+def baseUpdate(src, dst):
+    n_copied = 0
+    n_processed = 0
+    for R in src.keys():
+        id = R['id']
+        loc = R['loc']
+        if dst.has_item(id, loc):
+            print "%06d-%-6s skipped" % (id, loc)
+        else:
+            print "%06d-%-6s stored" % (id, loc)
+            dst.put(id, loc, src.get(id, loc))
+            n_copied += 1
+        n_processed += 1
+    print "done. processed: %d, copied: %d" % (n_processed, n_copied)
+    return
+
 
 def dumphdr(hdr):
     if QUIET: return
@@ -28,7 +169,7 @@ def dumphdr(hdr):
     return
 
 
-def fetch(region):
+def fetch(region, db):
     left = items_fetch_per_session
     if not QUIET:
         print "* fetching for %d unfetched items" % left
@@ -54,8 +195,9 @@ def fetch(region):
         iid = int(id)
         showed = False
         for loc in locales:
-            fname = "%s/%06d-%s.json" % (SAVEDIR, iid, loc)
-            if os.path.exists(fname):
+            if db.has_item(iid, loc):
+                #fname = "%s/%06d-%s.json" % (SAVEDIR, iid, loc)
+                #if os.path.exists(fname):
                 continue
             buf = cStringIO.StringIO()
             c.setopt(c.WRITEFUNCTION, buf.write)
@@ -78,7 +220,8 @@ def fetch(region):
                 print s
                 print "--------------------"
             
-            file(fname, "wt").write(s)
+            db.put(iid, loc, s)
+            #file(fname, "wt").write(s)
 #            r = s.replace(':false', ':False').replace(':true', ':True')
 #            V = eval(r)
             
@@ -88,4 +231,19 @@ def fetch(region):
     
 
 if __name__ == '__main__':
-    fetch(items_realms)
+    if False:
+        print "open old base"
+        db_old= FS_Base(SAVEDIR)
+        print "open new base"
+        db = D_Base(DBFILE)
+        print "update new base"
+        baseUpdate(db_old, db)
+        db_old.close()
+        db.close()
+        sys.exit(0)
+    
+    db = D_Base(DBFILE)
+    print "fetch new items to new base"
+    fetch(items_realms, db)
+    db.close()
+    print "done"
