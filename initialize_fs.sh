4a459fe87f6cfe5a54aa8ce28f1eeb23c68681e5
Lev Babchenko <lev@3divi.com>
2013-05-25 07:53:17 +0600

    zipping process was unified

--- initialize_fs.sh
+++ initialize_fs.sh
@@ -11,6 +11,7 @@ test -d $dir_fetching || mkdir -p $dir_fetching
 test -d $dir_fetched || mkdir -p $dir_fetched
 test -d $dir_importing || mkdir -p $dir_importing
 test -d $dir_imported || mkdir -p $dir_imported
+test -d $dir_zipping || mkdir -p $dir_zipping
 test -d $dir_zipped || mkdir -p $dir_zipped
 test -d $dir_log || mkdir -p $dir_log
 
